8c118f11751471f4fc46e4673f51c3de78a85194
Muhammad Yaris Ahyadi <107247604+yarisahyadi@users.noreply.github.com>
2026-06-12 16:08:29 +0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,66 @@
+import random
+
+squad_list = {
+    1: {
+        "First Name": "Mandla",
+        "Last Name": "Dlamini",
+        "Occupation": "Finch Squadron Commander",
+        "Nation": "South Africa",
+        "Followers": 100000
+        },
+    2: {
+        "First Name": "Jingyi",
+        "Last Name": "Zhang",
+        "Occupation": "Finch Squadron Vice Commander",
+        "Nation": "China",
+        "Followers": 120000
+    },
+    3: {
+        "First Name": "Ludwig",
+        "Last Name": "",
+        "Occupation": "Finch Squadron Scientist",
+        "Nation": "Germany",
+        "Followers": 180000
+    },
+    4: {
+        "First Name": "Anya",
+        "Last Name": "",
+        "Occupation": "Finch Squadron Scientist",
+        "Nation": "Ireland",
+        "Followers": 110000
+    },
+    5: {
+        "First Name": "Amira",
+        "Last Name": "Saeef",
+        "Occupation": "Finch Squadron Medic",
+        "Nation": "Egypt",
+        "Followers": 95000
+    },
+    6: {
+        "First Name": "Oliver",
+        "Last Name": "",
+        "Occupation": "Finch Squadron Pilot",
+        "Nation": "United States of America",
+        "Followers": 980000
+    },
+    7: {
+        "First Name": "Camilla",
+        "Last Name": "Vargas",
+        "Occupation": "Finch Squadron Engineer/Pilot",
+        "Nation": "Chile",
+        "Followers": 88000
+    },
+    8: {
+        "First Name": "Rohan",
+        "Last Name": "",
+        "Occupation": "Finch Squadron Engineer",
+        "Nation": "South Africa",
+        "Followers": 75000
+    }
+}
+
+# number = random.randint(1, 8)
+# data1 = "First Name"
+# data2 = "Followers"
+# print(f"\nRetrieving follower data for squad member no.{number}:\n{squad_list[number][data1]} - {squad_list[number][data2]}")
+# print(len(squad_list))
