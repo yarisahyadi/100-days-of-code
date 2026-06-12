5861f3dbdd0e61db74d0d85189fe18dfe995cf52
Muhammad Yaris Ahyadi <107247604+yarisahyadi@users.noreply.github.com>
2026-06-12 16:07:20 +0700

    Create higher_lower.py

---
+++ higher_lower.py
@@ -0,0 +1,31 @@
+import random
+import art
+import data
+
+def pick_subject(some_list):
+    """
+    choose a subject randomly
+    the list has keys of integer showing the member's ID
+    """
+    number = random.randint(1, len(some_list))
+    random_subject = some_list[number]
+    return random_subject
+
+def compare(subject_A, subject_B):
+    """
+    compare the followers between two subjects
+    """
+    if 
+    return
+
+# print the initial logo
+logo = art.logo
+print(logo)
+
+# reload the subject list to be compared
+subject_list = data.squad_list
+
+def main():
+    correct_guess = True
+    while correct_guess:
+        print(f"Compare A: {subject_list[]}")
